8de8d0d71d59e8f03c5a4b361ae5b68843fa8711
Vinusha Perera <VinushaP@99x.lk>
2019-10-30 14:35:27 +0530

    Updating ENVs

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,10 @@
 export const ENV: any = {
-    mode: 'Staging',
-    GATEWAY: 'https://tracified-gateway.herokuapp.com',
-    API_ADMIN: 'https://staging.admin.api.tracified.com',
-    APPCENTER_DEPLOYMENT_KEY: 'hzVMQ0zGn2fVTOMpj5EnsIcV35dxZKxY9FfzP',
-    BLOCKCHAIN_NETWORK: 'https://horizon-testnet.stellar.org',
-    NETWORK_TYPE: 'test'
-  };
+  mode: 'Staging',
+  API_TRACIFIED_V1: 'https://staging.api.tracified.com/api/v1',
+  API_TRACIFIED: 'https://staging.api.tracified.com/api/v2',
+  GATEWAY: 'https://tracified-gateway-staging.herokuapp.com',
+  API_ADMIN: 'https://staging.admin.api.tracified.com',
+  APPCENTER_DEPLOYMENT_KEY: 'hzVMQ0zGn2fVTOMpj5EnsIcV35dxZKxY9FfzP',
+  BLOCKCHAIN_NETWORK: 'https://horizon-testnet.stellar.org',
+  NETWORK_TYPE: 'test'
+};
