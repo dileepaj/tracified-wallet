b7019ead699f0896269d29c8dd5f59049b4d5ce4
chanukauk <cu9428gmail.com>
2022-01-18 11:19:39 +0530

    TRAC-3684 accept or reject  most recent recieved coc error fixing

--- src/pages/item-received/item-received.ts
+++ src/pages/item-received/item-received.ts
@@ -157,14 +157,6 @@ export class ItemReceivedPage {
   }
 
   updateCoC(coc, status) {
-    let count = this.pendingCoCCount(coc.sentOriginal, this.cocReceived);
-    console.log(count)
-    if(count > 0){
-      this.translate.get(['ERROR', 'ACC_REJECT_PENDING_COC', 'PENDING_COC']).subscribe(text => {
-        this.presentAlert("Error", text['ACC_REJECT_PENDING_COC '] + count + text[' PENDING_COC']);
-      });
-      return;
-    }
     this.passwordPromptResponseWait().then((password) => {
       this.presentLoading();
       this.blockchainService.validateTransactionPassword(password, this.mainAccount.sk, this.mainAccount.pk).then((decKey) => {
