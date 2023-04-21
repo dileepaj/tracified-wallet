7ad56a4d9a150386e7cf2f57ada9324e58b5c761
Ashvinmendi <100129123+Ashvinmendi@users.noreply.github.com>
2023-04-21 18:51:30 +0530

    TRAC-8194-fixed claim nft

--- src/app/pages/get-nft/get-nft.ts
+++ src/app/pages/get-nft/get-nft.ts
@@ -29,14 +29,6 @@ export class GetNftPage implements OnInit {
    imgrowlist = [];
    columCount;
    colSize;
-   // list: any = [
-   //    'https://tracified.sirv.com/Spins/RURI%20Gems/7776596590790/7776596590790.jpg',
-   //    'https://tracified.sirv.com/Spins/RURI%20Gems/7776596590790/7776596590790.jpg',
-   //    'https://tracified.sirv.com/Spins/RURI%20Gems/7776596590790/7776596590790.jpg',
-   //    'https://tracified.sirv.com/Spins/RURI%20Gems/7776596590790/7776596590790.jpg',
-   //    'https://tracified.sirv.com/Spins/RURI%20Gems/7776596590790/7776596590790.jpg',
-   //    'https://tracified.sirv.com/Spins/RURI%20Gems/7776596590790/7776596590790.jpg',
-   // ];
 
    constructor(private loadCtrl: LoadingController, public apiService: ApiServiceProvider, private translate: TranslateService, private logger: LoggerService, private router: Router) {}
    ngOnInit() {
@@ -60,19 +52,15 @@ export class GetNftPage implements OnInit {
          this.colSize = 2;
       }
       this.claimNft();
-      // let reverseArray = await this.reverseArray(this.list);
-      // this.splitImage(reverseArray);
    }
 
    claimNft() {
       this.apiService
          .getAllNft()
-         .then(async res => {
-            // let hash: string = this.list[0].nftcontent;
-            // this.getSVG(hash);
+         .then(async (res: any) => {
             console.log(res);
             if (res) {
-               let reverseArray = await this.reverseArray(res);
+               let reverseArray = await this.reverseArray(res.Response);
                this.splitImage(reverseArray);
             } else {
                await this.dissmissLoading();
@@ -125,16 +113,6 @@ export class GetNftPage implements OnInit {
             if (this.startloading) {
                this.dissmissLoading();
             }
-            // this.Decryption = res.Response;
-
-            // const svgString = this.Decryption.toString();
-            // console.log(svgString);
-
-            // const base64String = btoa(svgString);
-
-            // let imgElement = `data:image/svg+xml;base64,${base64String}`;
-            // this.imageSrc = imgElement;
-            // console.log(imgElement);
          },
          error => {
             if (this.startloading) {
@@ -150,68 +128,4 @@ export class GetNftPage implements OnInit {
       let url = 'https://stellar.expert/explorer/testnet/tx/' + hash;
       window.open(url, '_blank');
    }
-
-   // getOwnNFT() {
-   //    let assetOwn = [];
-   //    this.storage.getBcAccounts('mithilapanagoda@gmail.com').then((res1: any) => {
-   //       console.log('retieved result ', res1);
-   //       this.keypair = Keypair.fromSecret(res1);
-   //       console.log('Public Key is', this.keypair.publicKey().toString());
-   //    });
-   //    this.apiService
-   //       .retriveNFT('NOTFORSALE', this.mainAccount)
-   //       .then(a => {
-   //          console.log('size: ', a.body.length);
-   //          console.log('all data: ', a.body);
-   //          this.nft = a.body;
-   //          for (let x = 0; x < this.nft.length; x++) {
-   //             console.log('inside if condition');
-   //             this.apiService.getSVGByHash(this.nft[x].ImageBase64).subscribe((res: any) => {
-   //                console.log('reslts  svg: ', res);
-   //                this.Decryption = res.Response.Base64ImageSVG;
-   //                this.dec = btoa(this.Decryption);
-   //                var str2 = this.dec.toString();
-   //                var str1 = new String('data:image/svg+xml;base64,');
-   //                var src = str1.concat(str2.toString());
-   //                this.imageSrc = this._sanitizer.bypassSecurityTrustResourceUrl(src);
-   //                let NFTModel = {
-   //                   Owner: this.nft[x].CurrentOwnerNFTPK,
-   //                   Name: this.nft[x].NftContentName,
-   //                   Issuer: this.nft[x].InitialIssuerPK,
-   //                   Txn: this.nft[x].NFTTXNhash,
-   //                   ImageBase: this.imageSrc,
-   //                   Hash: this.nft[x].ImageBase64,
-   //                };
-   //                assetOwn.push(NFTModel);
-   //                console.log('data elements ', NFTModel);
-   //                this.ownNFTs = assetOwn;
-   //                console.log('list : ', this.ownNFTs);
-   //             });
-   //          }
-   //          if (this.isLoadingPresent) {
-   //             this.dissmissLoading();
-   //          }
-   //       })
-   //       .catch(error => {
-   //          this.logger.error("Couldn't retrive NFT from databse" + JSON.stringify(error));
-   //          if (this.isLoadingPresent) {
-   //             this.dissmissLoading();
-   //          }
-   //       });
-   // }
-
-   // LoadSVG(hash) {
-   //    console.log('hash: ', hash);
-   //    this.apiService.getSVGByHash(hash).subscribe((res: any) => {
-   //       this.Decryption = res.Response.Base64ImageSVG;
-   //       this.dec = btoa(this.Decryption);
-   //       var str2 = this.dec.toString();
-   //       var str1 = new String('data:image/svg+xml;base64,');
-   //       var src = str1.concat(str2.toString());
-   //       this.imageSrc = this._sanitizer.bypassSecurityTrustResourceUrl(src);
-   //       console.log('this image: ', this.imageSrc);
-   //       this.router.navigate(['./'], { state: this.imageSrc });
-   //       // this.navCtrl.push(PagesLoadSvgPage, { st: this.imageSrc });
-   //    });
-   // }
 }
