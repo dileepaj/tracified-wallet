0e1f7b682c53d94a0db47aa436862bd3b8245b37
Ashvinmendi <100129123+Ashvinmendi@users.noreply.github.com>
2023-04-25 13:01:22 +0530

    TRAC-8194-fixed issues

--- src/app/pages/item-received/item-received.module.ts
+++ src/app/pages/item-received/item-received.module.ts
@@ -1,29 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 import { ItemReceivedPage } from './item-received';
 import { CommonModule } from '@angular/common';
 
-export function createTranslateLoader(http: HttpClient) {
-   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-}
-
 @NgModule({
-   imports: [
-      TranslateModule.forRoot({
-         loader: {
-            provide: TranslateLoader,
-            useFactory: createTranslateLoader,
-            deps: [HttpClient],
-         },
-      }),
-      IonicModule,
-      RouterModule.forChild([{ path: '', component: ItemReceivedPage }]),
-      CommonModule,
-   ],
+   imports: [TranslateModule, IonicModule, RouterModule.forChild([{ path: '', component: ItemReceivedPage }]), CommonModule],
    declarations: [ItemReceivedPage],
    exports: [ItemReceivedPage],
 })
--- src/app/pages/transfer/transfer.module.ts
+++ src/app/pages/transfer/transfer.module.ts
@@ -1,32 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 import { TransferPage } from './transfer';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-export function createTranslateLoader(http: HttpClient) {
-   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-}
-
 @NgModule({
-   imports: [
-      TranslateModule.forRoot({
-         loader: {
-            provide: TranslateLoader,
-            useFactory: createTranslateLoader,
-            deps: [HttpClient],
-         },
-      }),
-      IonicModule,
-      RouterModule.forChild([{ path: '', component: TransferPage }]),
-      CommonModule,
-      FormsModule,
-   ],
+   imports: [TranslateModule, IonicModule, RouterModule.forChild([{ path: '', component: TransferPage }]), CommonModule, FormsModule],
    declarations: [TransferPage],
    exports: [TransferPage],
 })
