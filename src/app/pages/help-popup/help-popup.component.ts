38a51f1c07a4b9e07b7b84ae7599b6b08f505590
Anjula Jayasinghe <83394637+anjulasjay99@users.noreply.github.com>
2023-06-15 09:12:21 +0530

    TRAC-8670 create help popup component

---
+++ src/app/pages/help-popup/help-popup.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { Browser } from '@capacitor/browser';
+
+@Component({
+   selector: 'app-help-popup',
+   templateUrl: './help-popup.component.html',
+   styleUrls: ['./help-popup.component.scss'],
+})
+export class HelpPopupComponent implements OnInit {
+   @ViewChild('helpPopup') content: ElementRef;
+   @ViewChild('helpBtn') helpBtn: ElementRef;
+   @Input() data: any;
+
+   isOpen = false;
+   constructor() {}
+
+   ngOnInit() {}
+
+   presentPopover() {
+      this.isOpen = true;
+   }
+
+   @HostListener('document:click', ['$event'])
+   onClick(event) {
+      if (!this.content.nativeElement.contains(event.target) && !this.helpBtn.nativeElement.contains(event.target)) {
+         this.isOpen = false;
+      }
+   }
+
+   async openWebpage(url: string) {
+      await Browser.open({ url });
+      this.isOpen = false;
+   }
+}
