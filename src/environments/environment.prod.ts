a4f5958b00a5c1f69f8dd21fabb520e68de78ddb
Mithila Panagoda <mithilapanagoda@gmail.com>
2023-07-13 16:39:39 +0530

    TRAC-8965 removed comments in prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,15 +8,7 @@ export const ENV: any = {
    BLOCKCHAIN_NETWORK: 'https://horizon.stellar.org',
    NETWORK_TYPE: 'live',
    NFT_BACKEND: 'https://api.nft.tracified.com'
-//    mode: 'Qa',
-//    API_TRACIFIED_V1: 'https://qa.api.tracified.com/api/v1',
-//    API_TRACIFIED: 'https://qa.api.tracified.com/api/v2',
-//    GATEWAY: 'https://qa.gateway.tracified.com',
-//    API_ADMIN: 'https://qa.admin.api.tracified.com',
-//    APPCENTER_DEPLOYMENT_KEY: 'hzVMQ0zGn2fVTOMpj5EnsIcV35dxZKxY9FfzP',
-//    BLOCKCHAIN_NETWORK: 'https://horizon-testnet.stellar.org',
-//    NETWORK_TYPE: 'test',
-//    NFT_BACKEND: 'https://qa.api.nft.tracified.com',
+
 };
 
 export const TOAST_TIMER: any = {
