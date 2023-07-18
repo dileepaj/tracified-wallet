2567008965eeab5146870510f830592da8748c28
Anjula Jayasinghe <83394637+anjulasjay99@users.noreply.github.com>
2023-07-18 09:11:53 +0530

    TRAC-8976 bc accounts page ui changes

--- src/app/pages/bc-account/bc-account.ts
+++ src/app/pages/bc-account/bc-account.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController, ModalController, ToastController, LoadingController } from '@ionic/angular';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AlertController, ModalController, ToastController, LoadingController, IonModal } from '@ionic/angular';
 // import { AddAccountPage } from '../add-account/add-account';
 import { ApiServiceProvider } from '../../providers/api-service/api-service';
 import { ConnectivityServiceProvider } from '../../providers/connectivity-service/connectivity-service';
@@ -12,16 +12,28 @@ import { StorageServiceProvider } from 'src/app/providers/storage-service/storag
 import { BlockchainType, SeedPhraseService } from 'src/app/providers/seedPhraseService/seedPhrase.service';
 import { Keypair as StellerKeyPair } from 'stellar-base';
 import { Properties } from 'src/app/shared/properties';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Keypair } from 'stellar-sdk';
+import { MappingServiceProvider } from 'src/app/providers/mapping-service/mapping-service';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
    selector: 'page-bc-account',
    templateUrl: 'bc-account.html',
    styleUrls: ['./bc-account.scss'],
 })
 export class BcAccountPage implements OnInit {
+   @ViewChild(IonModal) modal: IonModal;
    loading;
    userAcc = [];
    isLoadingPresent: boolean;
+   passwordType: string = 'password';
+   passwordIcon: string = 'eye-off';
+   form: FormGroup;
+   public account;
+   public privateKey: string;
+   public keyDecrypted: boolean = false;
+   selectedAcc: any;
+   modal2Open: boolean = false;
 
    constructor(
       public router: Router,
@@ -33,8 +45,14 @@ export class BcAccountPage implements OnInit {
       public alertCtrl: AlertController,
       public dataService: DataServiceProvider,
       private storageService: StorageServiceProvider,
-      private properties: Properties
-   ) {}
+      private properties: Properties,
+      private translate: TranslateService,
+      private mappingService: MappingServiceProvider
+   ) {
+      this.form = new FormGroup({
+         password: new FormControl('', Validators.compose([Validators.required])),
+      });
+   }
    async ngOnInit() {
       let mnemonic = await this.storageService.getMnemonic();
       let rst = await this.storageService.getAllMnemonicProfiles();
@@ -48,6 +66,7 @@ export class BcAccountPage implements OnInit {
          };
          this.userAcc.push(index);
       }
+      console.log('userAcc', this.userAcc);
    }
 
    ionViewDidEnter() {
@@ -125,5 +144,51 @@ export class BcAccountPage implements OnInit {
 
    viewAccount(account) {
       this.router.navigate(['/account-details'], { state: { account: account } });
+   }
+
+   hideShowPassword() {
+      this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+      this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+   }
+
+   async decryptSecretKey(account: any) {
+      const password = this.form.get('password').value;
+      try {
+         const secretKey = await this.mappingService.decryptSecret(account.sk, password);
+         const pair = Keypair.fromSecret(secretKey.toString());
+         if (pair.publicKey() === account.pk) {
+            this.keyDecrypted = true;
+            this.privateKey = secretKey.toString();
+            await this.modal.dismiss();
+            this.modal2Open = true;
+         } else {
+            const text = await this.translate.get(['ERROR', 'INCORRECT_PASSWORD']).toPromise();
+            this.presentAlert(text['ERROR'], text['INCORRECT_PASSWORD']);
+         }
+      } catch (err) {
+         const text = await this.translate.get(['ERROR', 'INCORRECT_PASSWORD']).toPromise();
+         this.presentAlert(text['ERROR'], text['INCORRECT_PASSWORD']);
+      }
+      this.form.get('password').setValue('');
+   }
+
+   async presentAlert(title: string, message: string) {
+      let alert = await this.alertCtrl.create({
+         header: title,
+         message: message,
+         buttons: [
+            {
+               text: 'OK',
+               handler: data => {},
+            },
+         ],
+      });
+
+      alert.present();
+   }
+
+   public onWillDismiss(event: any) {
+      console.log('called');
+      this.modal2Open = false;
    }
 }
