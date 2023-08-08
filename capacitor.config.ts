980b25439962d33b51d019e7ced64dbd6a73795f
Anjula Jayasinghe <83394637+anjulasjay99@users.noreply.github.com>
2023-08-08 17:50:03 +0530

    TRAC-9204 scroll issue in import bc account screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
-  appId: 'com.tracified.app.wallet',
-  appName: 'Tracified wallet',
-  webDir: 'www',
-  bundledWebRuntime: false
+   appId: 'com.tracified.app.wallet',
+   appName: 'Tracified wallet',
+   webDir: 'www',
+   bundledWebRuntime: false,
+   plugins: {
+      Keyboard: {
+         resize: KeyboardResize.Body,
+         style: KeyboardStyle.Default,
+         resizeOnFullScreen: true,
+      },
+   },
 };
 
 export default config;
